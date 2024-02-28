580c92fdee773a6c72a5aff43bfd60a5f193da34
mehd-io <mehdi@mehd.io>
2024-02-28 16:08:32 +0000

    adding load_id for inc load

--- transform/pypi_metrics/models/pypi_daily_stats.sql
+++ transform/pypi_metrics/models/pypi_daily_stats.sql
@@ -1,24 +1,39 @@
+WITH pre_aggregated_data AS (
+    SELECT
+        timestamp :: date as download_date,
+        details.system.name AS system_name,
+        details.system.release AS system_release,
+        file.version AS version,
+        project,
+        country_code,
+        details.cpu,
+        CASE
+            WHEN details.python IS NULL THEN NULL
+            ELSE CONCAT(
+                SPLIT_PART(details.python, '.', 1),
+                '.',
+                SPLIT_PART(details.python, '.', 2)
+            )
+        END AS python_version
+    FROM
+        {{ source('external_source', 'pypi_file_downloads') }}
+    WHERE
+        download_date >= '{{ var("start_date") }}'
+        AND download_date < '{{ var("end_date") }}'
+)
+
 SELECT
-    timestamp :: date as download_date,
-    details.system.name AS system_name,
-    details.system.release AS system_release,
-    file.version AS version,
+    MD5(CONCAT_WS('|', download_date, system_name, system_release, version, project, country_code, cpu, python_version)) AS load_id,
+    download_date,
+    system_name,
+    system_release,
+    version,
     project,
     country_code,
-    details.cpu,
-    CASE
-        WHEN details.python IS NULL THEN NULL
-        ELSE CONCAT(
-            SPLIT_PART(details.python, '.', 1),
-            '.',
-            SPLIT_PART(details.python, '.', 2)
-        )
-    END AS python_version,
+    cpu,
+    python_version,
     COUNT(*) AS daily_download_sum
 FROM
-    {{ source('external_source', 'pypi_file_downloads') }}
-WHERE
-    download_date >= '{{ var("start_date") }}'
-    AND download_date < '{{ var("end_date") }}'
+    pre_aggregated_data
 GROUP BY
     ALL
