9572fce0e920516ad4d9293abcf76fb2a5751ab7
mehd-io <mehdi@mehd.io>
2024-09-06 09:32:51 +0000

    being able to inject database name also in dbt project

--- transform/pypi_metrics/models/pypi_daily_stats.sql
+++ transform/pypi_metrics/models/pypi_daily_stats.sql
@@ -1,3 +1,20 @@
+{% set database_name = var('database_name', 'duckdb_stats') %}
+
+-- workaround to be able to inject a variable and still use dbt_unit_testing
+{% if execute and flags.WHICH in ['test', 'compile'] %}
+    {# This block will only run during testing or compilation #}
+    {% set source_table = dbt_unit_testing.source(
+        'external_source' if var('data_source') == 'external_source' else 'duckdb_stats',
+        'pypi_file_downloads'
+    ) %}
+{% else %}
+    {# This block will run during normal model execution #}
+    {% set source_table = source(
+        'external_source' if var('data_source') == 'external_source' else database_name,
+        'pypi_file_downloads'
+    ) %}
+{% endif %}
+
 WITH pre_aggregated_data AS (
     SELECT
         timestamp :: date as download_date,
@@ -16,11 +33,7 @@ WITH pre_aggregated_data AS (
             )
         END AS python_version
     FROM
-        {% if var('data_source') == 'external_source' %}
-        {{ dbt_unit_testing.source('external_source', 'pypi_file_downloads') }}
-        {% else %}
-        {{ dbt_unit_testing.source('duckdb_stats', 'pypi_file_downloads') }}
-        {% endif %}
+        {{ source_table }}
     WHERE
         download_date >= '{{ var("start_date") }}'
         AND download_date < '{{ var("end_date") }}'
